77e14da47b29ddfef2986b81d4ac52df7fe81075
Mohamad825-prog <mhd.kh1272005@gmail.com>
2025-11-22 21:36:31 +0200

    Push 5

--- burgero/src/App.js
+++ burgero/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Routes, Route } from "react-router-dom";
 
-function App() {
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import OrderModal from './components/OrderModal';
+
+import HomePage from './pages/HomePage';
+import MenuPage from './pages/MenuPage';
+import MealsPage from './pages/MealsPage';
+import SpecialMenuPage from './pages/SpecialMenuPage';
+import TestimonialPage from './pages/TestimonialPage';
+import ContactPage from './pages/ContactPage';
+
+const App = () => {
+  const [isOrderOpen, setIsOrderOpen] = useState(false);
+
+  const openOrderModal = () => setIsOrderOpen(true);
+  const closeOrderModal = () => setIsOrderOpen(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar openOrderModal={openOrderModal} />
+
+      <Routes>
+        <Route path="/" element={<HomePage openOrderModal={openOrderModal} />} />
+        <Route path="/menu" element={<MenuPage />} />
+        <Route path="/meals" element={<MealsPage />} />
+        <Route path="/special" element={<SpecialMenuPage />} />
+        <Route path="/testimonial" element={<TestimonialPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+
+      <Footer />
+      <OrderModal isOpen={isOrderOpen} onClose={closeOrderModal} />
     </div>
   );
-}
+};
 
 export default App;
