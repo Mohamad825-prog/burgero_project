ac1a0a33bcfc95b5c49bc0c06a6542d827843323
Mohamad825-prog <mhd.kh1272005@gmail.com>
2025-11-22 21:35:39 +0200

    Push 4

---
+++ burgero/src/pages/HomePage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Hero from "../components/Hero";
+
+const HomePage = ({ openOrderModal }) => {
+    return <Hero openOrderModal={openOrderModal} />;
+};
+
+export default HomePage;
