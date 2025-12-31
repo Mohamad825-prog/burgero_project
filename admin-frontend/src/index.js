6e8368c0bcf7b2b2e61d1b0b1058c629c38a906d
Mohamad825-prog <mhd.kh1272005@gmail.com>
2025-12-31 14:10:53 +0200

    Pushhh

--- admin-frontend/src/index.js
+++ admin-frontend/src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/admin-frontend">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
